ff18fac0e01a5262f48b4ba6c4831024f6844fb8
DeYung Le <deyung@gmail.com>
2022-04-07 13:53:36 -0500

    Added json validation

--- models/cat.go
+++ models/cat.go
@@ -8,9 +8,9 @@ import (
 
 type Cat struct {
 	ID        uuid.UUID `json:"id"`
-	Name      string    `json:"name" gorm:"check:name <> ''"`
-	Breed     string    `json:"breed" gorm:"check:breed <> ''"`
-	Color     string    `json:"color" gorm:"check:color <> ''"`
-	Birthdate time.Time `json:"birthdate"`
-	Weight    int       `json:"weight" gorm:"check:weight > 0"`
+	Name      string    `json:"name" binding:"required,min=2,max=24" gorm:"check:name <> ''"`
+	Breed     string    `json:"breed" binding:"required,min=2,max=24" gorm:"check:breed <> ''"`
+	Color     string    `json:"color" binding:"required,min=2,max=24" gorm:"check:color <> ''"`
+	Birthdate time.Time `json:"birthdate" binding:"required"`
+	Weight    int       `json:"weight" binding:"required,gte=1,lt=100" gorm:"check:weight > 0"`
 }
